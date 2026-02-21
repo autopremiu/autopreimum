7921f3771e88e1ad8bf2c2b763cbbfb88c28b5f1
auto premium <comercialautopremium@gmail.com>
2026-02-21 08:49:17 -0500

    Instalado Brevo

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,21 +1,35 @@
-const { Resend } = require('resend');
+const SibApiV3Sdk = require('@getbrevo/brevo');
+require("dotenv").config();
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+apiInstance.setApiKey(
+  SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey,
+  process.env.BREVO_API_KEY
+);
 
 async function enviarEncuestaEmail(cliente, servicioId) {
 
     const linkEncuesta = `https://autopreimum.onrender.com/encuesta/${servicioId}`;
 
-    await resend.emails.send({
-        from: 'Autopremium <onboarding@resend.dev>',
-        to: cliente.email,
-        subject: 'Encuesta de Satisfacción - Taller',
-        html: `
+    const sendSmtpEmail = {
+        to: [{
+            email: cliente.email,
+            name: cliente.nombre
+        }],
+        sender: {
+            email: "comercialautopremium@gmail.com",
+            name: "Autopremium"
+        },
+        subject: "Encuesta de Satisfacción - Taller",
+        htmlContent: `
             <h2>Hola ${cliente.nombre}</h2>
             <p>Gracias por confiar en nuestro taller.</p>
             <a href="${linkEncuesta}">Responder Encuesta</a>
         `
-    });
+    };
+
+    await apiInstance.sendTransacEmail(sendSmtpEmail);
 }
 
 module.exports = enviarEncuestaEmail;
