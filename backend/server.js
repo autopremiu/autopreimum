b789d0774eb8f5f61d1e975a988eaefadbef7acd
auto premium <comercialautopremium@gmail.com>
2026-03-05 09:22:28 -0500

    se creo ruta de ping

--- backend/server.js
+++ backend/server.js
@@ -51,6 +51,10 @@ app.use((req, res) => {
     res.status(404).json({ message: "Ruta no encontrada" });
 });
 
+
+app.get("/ping", (req, res) => {
+  res.send("ok");
+});
 /* ==============================
    INICIAR SERVIDOR
 ============================== */
